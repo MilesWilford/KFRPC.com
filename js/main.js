b2bb28e1c0d890d21f8271b9035a399bca62425a
MilesWilford <miles.wilford@gmail.com>
2013-04-28 15:43:45 -0400

    Text correction, improved javascript

--- js/main.js
+++ js/main.js
@@ -1,17 +1,24 @@
 $(document).ready(function() {
     $('.lang-rows .tabbed h2').click(function() {
         $this = $(this);
+        var tab = $this.attr('id');
+
         $('.lang-rows .tabbed h2').removeClass('current');
         $this.addClass('current');
-        if ($this.attr('id') == 'tab-en') {
-            $('.lang-rows section').addClass('hidden');
-            $('section[lang=en]').removeClass('hidden');
-        } else if ($this.attr('id') == 'tab-de') {
-            $('.lang-rows section').addClass('hidden');
-            $('section[lang=de]').removeClass('hidden');
-        } else if ($this.attr('id') == 'tab-fr') {
-            $('.lang-rows section').addClass('hidden');
-            $('section[lang=fr]').removeClass('hidden');
+        $('.lang-rows section').addClass('hidden');
+
+        switch (tab) {
+            case 'tab-en':
+                $('section[lang=en]').removeClass('hidden');
+                break;
+            case 'tab-de':
+                $('section[lang=de]').removeClass('hidden');
+                break;
+            case 'tab-fr':
+                $('section[lang=fr]').removeClass('hidden');
+                break;
+            default:
+                console.log('Something bad happened - tab does not exist');
         }
     });
 });
