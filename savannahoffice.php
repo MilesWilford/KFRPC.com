a35c5a82ee04c85c1c567e1bd39356c4d6332425
MilesWilford <miles.wilford@gmail.com>
2013-04-30 10:57:54 -0400

    Added new php files

---
+++ savannahoffice.php
@@ -0,0 +1,42 @@
+<?php include ('includes/header.html'); ?>
+<div class="main">
+    <h1>Our Savannah Office</h1>
+    <section class="staff">
+        <h2>Andrew Wilford, Senior Partner</h2>
+        <aside><img src="img/Andrewsm.jpg" alt="Andrew Wilford, headshot" />
+        </aside>
+        <p>Mr. Wilford has an AB in languages from Rutgers University (1967) and studied for three years at The Newark College of Engineering (now the NJ Institute of Technology).</p>
+        <p>He received a JD in law from Seton Hall Law School (1975).</p>
+        <p>He has been associated with <strong>KF ROSS PC</strong> since 1968 and specializes in electrical matters and mechanics as well as operating the firm.</p>
+        <p>Mr. Wilford speaks fluent French and German and has a reading knowledge of Italian and Spanish.</p>
+    </section>
+    <section class="staff">
+        <h2>Our Savannah staff</h2>
+        <ul>
+            <li>Brooke Wilford: Office &amp; Projects Manager</li>
+            <li>Rolanda Powell: Administrative Assistant &amp; Communication Manager</li>
+        </ul>
+        <aside><img src="img/sav_xmas_2011.jpg" alt="Savannah Office, Chrismas 2011" />
+        </aside>
+    </section>
+    <section class="vcard">
+        <div>
+            <a class="url fn n org" href="http://kfrpc.com"> K.F. Ross P.C. </a>
+        </div>
+        <div class="adr">
+            <div class="street-address">
+                311 East York Street
+            </div>
+            <span class="locality">Savannah</span>, <span class="region">GA</span>, <span class="postal-code">31401</span>
+            <span class="country-name">United States</span>
+            <div class="email">
+                Email:
+                <a href="mailto:savannah@kfrpc.com">savannah@kfrpc.com</a>
+            </div>
+            <div class="tel">
+                <span class="type">Fax</span> +1-912-344-4472
+            </div>
+        </div>
+    </section>
+</div>
+<?php include ('includes/footer.html'); ?>
